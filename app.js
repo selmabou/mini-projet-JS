6e57982d5a65c935b431524d1fd7e1f9c6699055
selmabou <bouabidisalma07@gmail.com>
2024-01-31 00:18:01 +0100

    random commit

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ function Vider() {
   Table = [];
   
 }
+
+function random() {
+  if (Table.length > 0) {
+    var motAleatoire = Table[Math.floor(Math.random() * Table.length)];
+    var resultatParagraphe = document.getElementById('resultat');
+    resultatParagraphe.innerHTML =  motAleatoire;
+  } else {
+    alert("Ajoutez des mots !!");
+  }
+}
